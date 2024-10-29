28374b9323a983febc566d30e658753329cf983a
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-10-29 18:04:35 +0400

    js tasks

---
+++ javascript-tasks/task1.js
@@ -0,0 +1,203 @@
+// let age = prompt("Zehmet olmasa yashinizi daxil  edin.")
+// if(age>=0){
+//   if(age>=65){
+//     alert("Pensiya uchun uygunsunuz")
+//   }
+//   else{
+//     alert("Pensiya uchun uygun deyilsiniz")
+//   }
+
+// }
+
+// else{
+//   alert("Yashiniz duzgun deyil")
+// }
+
+// task 2
+
+// let teref1 = prompt("1-ci terefi daxil edin: ");
+// let teref2 = prompt("2-ci terefi daxil edin: ");
+// let teref3 = prompt("3-cu terefi daxil edin: ");
+// if(teref1==teref2 && teref2==teref3){
+//   alert("beraberterefli uchbucag")
+// }
+// else{
+//   alert("muxtelifterefli uchbucag")
+// }
+
+// task3
+
+// let number = prompt("enter the number: ");
+// if(number>0){
+//   alert("is positive")
+// }
+// else{
+//   alert("is negative")
+// }
+
+// task4
+
+// let month = +prompt("enter the month: ");
+// switch (month) {
+//   case 1: alert("january")
+//      break;
+
+//     case 2: alert("february")
+//     break;
+
+//     case 3: alert("march")
+//     break;
+
+//     case 4: alert("april")
+//     break;
+
+//     case 5: alert("may")
+//     break;
+
+//     case 6: alert("june")
+//     break;
+
+//     case 7: alert("july")
+//     break;
+
+//     case 8: alert("august")
+//     break;
+
+//     case 9: alert("september")
+//     break;
+
+//     case 10: alert("october")
+//     break;
+
+//     case 11: alert("november")
+//     break;
+
+//     case 12: alert("december")
+//     break;
+
+//   default: alert("enter the correct month")
+//     break;
+
+// }
+
+// task5
+
+// let number = prompt("enter the number: ");
+// if(number>=0){
+//   if(number%15==0 ){
+//     alert("true")
+//   }
+// }
+// else{
+//   alert("enter the correct number: ")
+// }
+
+// task6
+
+// let number = prompt("enter the number: ");
+// if(number%2==0){
+//   alert("bolundu")
+// }
+// else{
+//   alert("enter the correct number")
+// }
+
+// task7
+
+// let number1 = prompt("enter the first number: ");
+// let number2 = prompt("enter the second number: ");
+// if(number1*number2>0){
+//   alert("is positive")
+// }
+// else{
+//   alert("is negative")
+// }
+
+// task8
+
+// let number = prompt("enter the number: ");
+// if (number>1 && number<100){
+//   alert("correct number")
+// }
+// else{
+//   alert("enter the another number")
+// }
+
+// task9
+
+// let number1 = prompt("enter the first number: ");
+// let number2 = prompt("enter the second number: ");
+// let number3 = prompt("enter the third number: ");
+// if(number1>number2 && number1>number3){
+//   alert("the biggest number is number1")
+// }
+// else if(number2>number1 && number2>number3){
+//   alert("the biggest number is number2")
+// }
+// else if(number3>number1 && number3>number2){
+//   alert("the biggets number is number3")
+// }
+
+// task10
+
+// let score = +prompt("enter the student score: ");
+// if(score>90){
+//   alert("A")
+// }
+// else if(score>80 && score<90){
+//   alert("B")
+// }
+// else if(score>70 && score<80){
+//   alert("C")
+// }
+// else if(score>60 && score<70){
+//   alert("D")
+// }
+// else{
+//   alert("F")
+// }
+
+// task11
+
+// let age = prompt("enter your age: ");
+// if(age>0 && age<18){
+//   alert("Yeniyetme")
+// }
+// else if(age>18 && age<65){
+//   alert("Yetkin")
+// }
+// else{
+//   alert("Yashli")
+// }
+
+// task11
+
+// let time = +prompt("enter the time: ");
+// if(time>6 && time<=12){
+//   alert("Sabahiniz xeyir")
+// }
+// else if(time>12 && time<=17){
+//   alert("Gunortaniz xeyir")
+// }
+// else if(time>17 && time <25){
+//   alert("Axshaminiz xeyir")
+// }
+
+// task12
+
+let x = +prompt("enter the x value");
+let y = +prompt("enter the y value");
+if(x>0 && y<0){
+  alert(4*x + 2*y + 4)
+}
+else if(x>0 && y==0){
+  alert( 2*x - y+3 )
+}
+else if(x<0 && y>0){
+  alert(3*x + 4*y+3 )
+}
+
+
+
+
+
