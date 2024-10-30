4e59bc63e02dd87113880a80c18dfc790f5e789d
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-10-30 19:27:16 +0400

    30.10 loops

--- javascript-tasks/task1.js
+++ javascript-tasks/task1.js
@@ -185,17 +185,193 @@
 
 // task12
 
-let x = +prompt("enter the x value");
-let y = +prompt("enter the y value");
-if(x>0 && y<0){
-  alert(4*x + 2*y + 4)
-}
-else if(x>0 && y==0){
-  alert( 2*x - y+3 )
-}
-else if(x<0 && y>0){
-  alert(3*x + 4*y+3 )
-}
+// let x = +prompt("enter the x value");
+// let y = +prompt("enter the y value");
+// if(x>0 && y<0){
+//   alert(4*x + 2*y + 4)
+// }
+// else if(x>0 && y==0){
+//   alert( 2*x - y+3 )
+// }
+// else if(x<0 && y>0){
+//   alert(3*x + 4*y+3 )
+// }
+
+
+// 30.10 TASKS
+// task1
+// for (let i = 0; 1 <=100; i++) {
+//  if (i%2 == 0){
+//   console.log(i);
+//  }
+//   }
+
+ // task2
+// let count =0
+// for (let i = 0; 1 <=100; i++) {
+//   if (i%2 == 1){
+//     count++
+//   }
+   
+//  }console.log(count);
+
+ // task3
+// let sum =0
+//  for (let i = 0; 1 <=100; i++) {
+//   if  (i%5 == 0){
+//     sum+= i
+//    console.log(i);
+//   }
+   
+//  }
+
+//  task4-#1
+//  let arr =[12,14,35,56,68,93]
+//  let sum =0
+//  for (let i = 0; i < arr.length; i++) {
+//   sum+= arr[i]
+//  }
+//  console.log(sum);
+
+// #2
+//  for (let i = 0; i < arr.length; i++) {
+//   if(i%2==1){
+//     console.log(arr[i]);
+//   }
+//  }
+
+ //  #3
+// for (let i = 0; i < arr.length; i+2) {
+//   console.log(arr[i]);
+// }
+
+// #4
+// let biggest = array[0]
+// for (let i = 0; i < array.length; i++) {  
+//   if(array[i]>biggest){
+//     biggest = array[i]
+//   }
+// } 
+// console.log(biggest);
+
+// #5
+// let biggest = array[0]
+// let smallest = array[0]
+// for (let i = 0; i < array.length; i++) {  
+//   if(array[i]>biggest){
+//     biggest = array[i]
+//   }
+//   if(array[i]<smallest){
+//     smallest = array[i]
+//   }
+// } 
+// console.log(biggest - smallest);
+
+// #6
+// let number = prompt("enter the number")
+// let array =[11,14,34,86,95,23]
+// for (let i = 0; i < array.length; i++) {
+//   if(number = array[i]){
+//     alert(`${array[i]} array'e daxildir`)
+//   }
+// }
+
+// #7
+// const employees = [
+//   { name: "Jamil", salary : 50000, department: "IT" },
+//   { name: "Jale", salary: 60000, department: "HR" },
+//   { name: "Bayram", salary: 55000, department: "IT" },
+//   { name: "Sahil", salary: 75000, department: "HR" },
+//   { name: "Maryam", salary: 65000, department: "IT" },
+//   { name: "Elnara", salary: 80000, department: "HR" },
+//   { name: "Davud", salary: 70000, department: "IT" },
+// ]
+// for (let i = 0; i < employees.length; i++) {
+//   if(employees[i].salary>60000){
+//     console.log(employees[i].name);
+//   }
+// }
+
+// #8
+// let number = +prompt("enter the number: ")
+// let factorial = 1;
+// for (let i = 1; i <= number; i++) {
+//   factorial = factorial*i
+  
+// }
+// alert(factorial);
+
+// #9
+// let str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, qu";
+// let count = 0;
+// for (let i = 0; index < str.length; i++) {
+//   if(str[i]=-"o"){
+//     count++
+//   }
+  
+// }
+// console.log(count);
+
+// #10
+// const countries = ["azerbaijan", "norway", "germany", "albania", "america"]
+// for (let i = 0; i < countries.length; i++) {
+//   if(countries[i][0]= "a"){
+//     console.log(countries[i]);
+//   }
+  
+// }
+
+// for (let i = 0; i < countries.length; i++) {
+//   if(countries[i][0]= "a" && countries[i][countries[i].length-1]){
+//     console.log(countries[i]);
+//   }
+  
+// }
+
+// let n = 427;
+// while (n>0) {
+//   let reminder = n % 10;
+//   console.log(reminder);
+//   number = (number - reminder) / 10;
+// }
+
+// let number = 753
+// let isPrime = true;
+// for (let i = 2; i < Math.sqrt(number); i++) {
+//   if(number % i === 0){
+//    isPrime = false;
+//    break;
+//   }
+  
+// }
+// isPrime === true
+// ? console.log(`${number} is prime`)
+// : console.log(`${number} is NOT prime`);
+
+
+// const scores = [60, 75, 88, 75, 93, 45]
+// for (let i = 0; i < scores.length; i++) {
+//   sum= sum + scores[i]
+// }
+// console.log(sum / scores.length);
+
+// for (let i = 0; i < scores.length; i++) {
+//   if(scores[i]> scores[i-1]){
+//     max = scores[i]
+//   }
+// }
+
+// console.log(max);
+
+
+
+
+
+
+
+ 
+
+
 
 
 
